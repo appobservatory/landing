4f16dd6a1a975955c4742c1aa711e3e4b619e430
Garrett Dengler <garrett@dnglr.com>
2022-03-15 09:20:20 -0400

    favicons

--- source/_layouts/main.blade.php
+++ source/_layouts/main.blade.php
@@ -7,6 +7,10 @@
   <link rel="canonical" href="{{ $page->getUrl() }}">
   <meta name="description" content="{{ $page->description }}">
   <meta name="keywords" content="{{ $page->keywords }}">
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+  <link rel="manifest" href="/site.webmanifest">
   <title>{{ $page->title }}</title>
   <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
   <script defer src="{{ mix('js/main.js', 'assets/build') }}"></script>
