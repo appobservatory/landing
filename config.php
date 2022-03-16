33a3f3c09de3fb086476295a2ff6ee7e8117b5bc
Garrett Dengler <garrett@dnglr.com>
2022-03-16 09:37:31 -0400

    solarwinds blog post start

--- config.php
+++ config.php
@@ -4,8 +4,8 @@ return [
   'production' => false,
   'baseUrl' => 'https://appobservatory.com',
   'title' => 'Web Application Monitoring | App Observatory',
-  'description' => 'Monitor your web application for availability, response time, and dependency updates.',
-  'keywords' => 'web application, monitoring, updates, apps, up time',
+  'description' => 'Monitor your web application for availability, site up-time, page response time, and dependency updates.',
+  'keywords' => 'web application, monitoring, updates, apps, page response time, site uptime monitoring, app observatory, pingdom, solarwinds',
   'collections' => [
     'updates' => [
       'path' => '/updates/{-title}',
---
+++ source/_updates/2-app-observatory-vs-pingdom.md
@@ -0,0 +1,12 @@
+---
+extends: update
+section: updateContent
+title: "App Observatory vs Solarwinds Pingdom"
+author: "Garrett"
+date: "2022-03-16"
+heading: "Why choose App Observatory over another uptime monitoring platform like Solarwinds Pingdom?"
+---
+
+Solarwinds Pingdom is a well-established software-as-a-service that offers uptime monitoring and a lot more. 
+While App Observatory is significantly small with less features, we make up for the difference with cost and 
+ease-of-use. 
