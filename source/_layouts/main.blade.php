c0ddaf2078ce406ef7cc34b0acb9110a64d5bdfd
Garrett Dengler <garrett@dnglr.com>
2022-03-17 21:04:16 -0400

    microsoft clarity

--- source/_layouts/main.blade.php
+++ source/_layouts/main.blade.php
@@ -29,6 +29,14 @@
     })();
   </script>
   <!-- End Matomo Code -->
+  <!-- Clarity tracking code for https://appobservatory.com/ -->
+  <script>
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "b2qz7z3txo");
+  </script>
 </head>
 
 <body class="text-gray-900 font-sans antialiased min-h-full">
