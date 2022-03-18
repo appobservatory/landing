96a49ea843237f229d35cebab61b82fbcc6ba9d0
Garrett Dengler <garrett@dnglr.com>
2022-03-18 08:10:24 -0400

    nav and robots

--- source/_layouts/simple.blade.php
+++ source/_layouts/simple.blade.php
@@ -1,6 +1,11 @@
 @extends('_layouts.main')
 @section('body')
-<div class="mt-6 prose prose-indigo prose-lg text-gray-500 mx-auto">
+<div class="text-center space-y-4 max-w-7xl mx-auto py-16 md:grid md:grid-cols-3 md:items-center md:space-y-0">
+  <a class="text-base font-semibold text-indigo-600 hover:text-indigo-500" href="/">&LeftArrow; Back Home</a>
+  <img class="mx-auto block h-8 w-auto" src="/assets/images/logo-full.svg" alt="App Observatory">
+  <div></div>
+</div>
+<div class="py-16 mt-6 prose prose-indigo prose-lg text-gray-500 mx-auto">
   @yield('content')
 </div>
 @endsection
